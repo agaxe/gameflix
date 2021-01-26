e2b40d960e65a5994a1ccdb1875cba2e17339d99
agaxe <agaxe@naver.com>
2021-01-26 10:05:04 +0900

    FIX : [atoms/item] data 속성값 추가
    - item 컴포넌트에 data-name, data-value 속성을 추가하였습니다.

--- components/atoms/Item/index.tsx
+++ components/atoms/Item/index.tsx
@@ -8,15 +8,21 @@ type ItemProps = {
 	children: React.ReactNode;
 	/** 클릭 이벤트 */
 	onClick?: (e?: any) => void;
+	/** data name*/
+	dataName?: string | number;
+	/** data value */
+	dataValue?: string | number;
 }
 
 //* component
 /**
  * - `<li>` 태그를 사용한 Item 컴포넌트 입니다.
  */
-function ItemComp({ className, children, onClick }: ItemProps) {
+function ItemComp({ className, children, onClick, dataName, dataValue }: ItemProps) {
 	return (
 		<li
+			data-name={dataName}
+			data-value={dataValue}
 			className={className}
 			onClick={onClick}
 		>
