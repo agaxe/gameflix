1358c22d24cb1d09e514072c7c87b9322779f077
agaxe <agaxe@naver.com>
2021-01-26 10:09:09 +0900

    FIX : [page/_document] material ui ssr 오류 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
 		const sheet = new ServerStyleSheet()
+		const sheets = new ServerStyleSheets();
 		const originalRenderPage = ctx.renderPage
 
 		try {
 			ctx.renderPage = () =>
 				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(<App {...props} />),
+					enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
 				})
 
 			const initialProps = await Document.getInitialProps(ctx)
@@ -20,6 +22,7 @@ export default class MyDocument extends Document {
 					<>
 						{initialProps.styles}
 						{sheet.getStyleElement()}
+						{[...React.Children.toArray(initialProps.styles), sheets.getStyleElement()]}
 					</>
 				),
 			}
