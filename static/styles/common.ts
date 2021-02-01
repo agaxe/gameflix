641b585a73276499a26e37734b2060376e60591b
agaxe <agaxe@naver.com>
2021-02-01 15:23:44 +0900

    ADD : 페이지 타이틀 하단 영역 공통 스타일 추가
    - 페이지 타이틀 하단 영역의 공통 스타일을 추가하였습니다.

--- static/styles/common.ts
+++ static/styles/common.ts
@@ -1,6 +1,7 @@
 import { css } from 'styled-components';
-import { VAR_SIZE } from './variable';
+import { VAR_SIZE, VAR_COLOR } from './variable';
 const { BASE_FONT_SIZE } = VAR_SIZE;
+const { COLOR_PRIMARY } = VAR_COLOR
 
 // 박스 ( 카드 ) 그림자 효과
 export const STYLE_BOX_SHADOW = css`
@@ -16,3 +17,18 @@ export const STYLE_ELLIPSIS = css`
 export const pxToRem = (px) => {
 	return `${px / BASE_FONT_SIZE}rem`;
 }
+// 페이지 타이틀 하단 영역
+export const PAGE_TITLE_BOTTOM = css`
+	position:relative;
+	display:flex;
+	justify-content:space-between;
+	padding:35px 0;
+	& > *:first-of-type{
+		font-size:${pxToRem(30)};
+		word-break: keep-all;
+		line-height:1.2;
+		& > strong{
+			color:${COLOR_PRIMARY};
+		}
+	}
+`
