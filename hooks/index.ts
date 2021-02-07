d48954d3b6a94cf9790afce6f5ab4315db332faa
agaxe <agaxe@naver.com>
2021-02-07 21:39:15 +0900

    ADD : useGameApi & useTwitchApi 생성
    - IGDB API 와 Twitch API 를 사용할 수 있는 커스텀 훅을 생성 하였습니다.

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,2 +1,4 @@
+export { default as useGameApi } from './useGameApi';
 export { default as useInput } from './useInput';
+export { default as useTwitchApi } from './useTwitchApi';
 
---
+++ hooks/useTwitchApi.tsx
@@ -0,0 +1,25 @@
+import Axios from 'axios';
+
+export default async function useGameApi(endPoint: string) {
+
+	// 공통 변수
+	const TWITCH_ACCESS_TOKEN_URL = process.env.TWITCH_ACCESS_TOKEN_URL;
+	const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+
+	// access_token
+	const access_token = await Axios.post(TWITCH_ACCESS_TOKEN_URL).then(res => res.data.access_token);
+
+	const result = await Axios({
+		url: `https://api.twitch.tv/helix/${endPoint}`,
+		method: "get",
+		headers: {
+			Accept: "application/json",
+			"Client-ID": TWITCH_CLIENT_ID,
+			Authorization: `Bearer ${access_token}`
+		}
+	})
+	return result.data;
+}
+
+
+
