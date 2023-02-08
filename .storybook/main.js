7e000dad82bff0f6da6a5fabe20508b12e497445
agaxe <agaxe.dev@gmail.com>
2023-02-08 14:52:07 +0900

    UPDATE: storybook 설정 파일 임시 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,22 +2,23 @@ const path = require('path');
 
 module.exports = {
   stories: ['../components/**/**/*.stories.@(js|mdx|ts|tsx)'],
+  framework: '@storybook/react',
   addons: [
     '@storybook/addon-knobs',
     '@storybook/addon-actions',
-    '@storybook/addon-docs',
+    '@storybook/addon-docs'
   ],
 
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
+      include: path.resolve(__dirname, '../')
     });
-    config.resolve.modules = [
-      path.resolve(__dirname, "../"),
-      "node_modules",
-    ]
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../')
+    };
     return config;
-  },
+  }
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,31 +1,33 @@
-import React, { useEffect } from "react";
-import { RouterContext } from 'next/dist/next-server/lib/router-context';
-import { css, createGlobalStyle } from "styled-components";
+import React, { useEffect } from 'react';
+//import { AppRouterContext } from 'next/dist/shared/lib/app-router-context';
+import { createGlobalStyle, css } from 'styled-components';
 import { global, reset } from '../static/styles';
 
-// ? 스토리북 마크다운 스타일 
+// ? 스토리북 마크다운 스타일
 const MarkdownStyle = css`
-// 리스트 disc
-.sbdocs{
-	list-style:disc !important;
-}
-`
+  // 리스트 disc
+  .sbdocs {
+    list-style: disc !important;
+  }
+`;
 // ? 글로벌 스타일
 const GlobalStyle = createGlobalStyle`
 	${global}
 	${reset}
 	${MarkdownStyle}
-`
+`;
 
 export const decorators = [
-	(Story) => (
-		<RouterContext.Provider value={{
-			push: () => Promise.resolve(),
-			replace: () => Promise.resolve(),
-			prefetch: () => Promise.resolve()
-		}}>
-			<GlobalStyle />
-			<Story />
-		</RouterContext.Provider>
-	),
+  (Story) => (
+    // <RouterContext.Provider value={{
+    // 	push: () => Promise.resolve(),
+    // 	replace: () => Promise.resolve(),
+    // 	prefetch: () => Promise.resolve()
+    // }}>
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+    // </RouterContext.Provider>
+  )
 ];
