c6571b8a188e18d1f54755ddfdf04496ae20b122
agaxe <agaxe.dev@gmail.com>
2023-03-03 18:08:35 +0900

    CHORE: 불필요한 패키지 제거

--- next.config.js
+++ next.config.js
@@ -2,40 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
-
-// const Dotenv = require("dotenv-webpack");
-// const withSass = require('@zeit/next-sass');
-// const withCss = require('@zeit/next-css');
-
-// module.exports = withCss(withSass({
-// 	webpack: (config, { isServer }) => {
-// 		config.plugins.push(new Dotenv({ silent: true }));
-// 		if (!isServer) {
-// 			config.node = {
-// 				fs: 'empty'
-// 			}
-// 		}
-// 		config.module.rules.push({
-// 			test: /\.(png|jpe?g|gif|jp2|webp|svg)$/,
-// 			loader: 'file-loader',
-// 			options: {
-// 				name: 'static/images/[name].[ext]'
-// 			}
-// 		})
-
-// 		return config
-// 	},
-// 	env: {
-// 		TWITCH_ACCESS_TOKEN_URL: process.env.TWITCH_ACCESS_TOKEN_URL,
-// 		TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-// 		TWITCH_SECRET_ID: process.env.TWITCH_SECRET_ID,
-// 		IGDB_COVER_URL: process.env.IGDB_COVER_URL,
-// 		NO_COVER_IMAGE: process.env.NO_COVER_IMAGE,
-// 		SITE_KO_NAME: process.env.SITE_KO_NAME,
-// 		SITE_EN_NAME: process.env.SITE_EN_NAME,
-// 	},
-// }));
