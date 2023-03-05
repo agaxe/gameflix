6503b15361f68fba2dc87e9a2a1251e42e363f96
agaxe <agaxe.dev@gmail.com>
2023-03-05 12:19:39 +0900

    FIX: _document.js 관련 경고 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
-//import Head from 'next/head';
+import Head from 'next/head';
 import router from 'next/router';
-//import { VAR_COLOR } from '@/static/styles/variable';
 import { createGlobalStyle } from 'styled-components';
 import type {} from 'styled-components/cssprop';
-//import { SITE_KO_NAME } from '@/common/variables';
 import { LoadingModal } from '@/components/molecules/LoadingModal';
 import { Footer } from '@/components/organisms/Footer';
 import { Header } from '@/components/organisms/Header';
-//import dotenv from 'dotenv';
+import { SITE_KO_NAME } from '@/common/variables';
 import { global, reset } from '@/static/styles';
+import { VAR_COLOR } from '@/static/styles/variable';
 
-//const { COLOR_PRIMARY, COLOR_BLACK } = VAR_COLOR;
-
-//dotenv.config();
+const { COLOR_BLACK } = VAR_COLOR;
 
 // * common style
 const GlobalStyle = createGlobalStyle`
@@ -46,13 +43,22 @@ function App({ Component, pageProps }: AppProps) {
   const { statusCode } = pageProps;
 
   return (
-    <div id='wrap'>
-      {LoadingState ? <LoadingModal /> : null}
-      {!statusCode && <GlobalStyle />}
-      {!statusCode && <Header />}
-      <Component {...pageProps} />
-      {!statusCode && <Footer />}
-    </div>
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>{SITE_KO_NAME}</title>
+        <meta name='msapplication-TileColor' content={COLOR_BLACK} />
+        <meta name='theme-color' content={COLOR_BLACK} />
+      </Head>
+      <div id='wrap'>
+        {LoadingState ? <LoadingModal /> : null}
+        {!statusCode && <GlobalStyle />}
+        {!statusCode && <Header />}
+        <Component {...pageProps} />
+        {!statusCode && <Footer />}
+      </div>
+    </>
   );
 }
 export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import { ServerStyleSheet } from 'styled-components';
-import { SITE_KO_NAME } from '@/common/variables';
 import { VAR_COLOR } from '@/static/styles/variable';
 
-const { COLOR_PRIMARY, COLOR_BLACK } = VAR_COLOR;
+const { COLOR_PRIMARY } = VAR_COLOR;
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -48,11 +47,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang='ko'>
         <Head>
-          <meta charSet='UTF-8' />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap'
@@ -82,9 +76,6 @@ export default class MyDocument extends Document {
             href='/static/images/favicon/safari-pinned-tab.svg'
             color={COLOR_PRIMARY}
           />
-          <meta name='msapplication-TileColor' content={COLOR_BLACK} />
-          <meta name='theme-color' content={COLOR_BLACK} />
-          <title>{SITE_KO_NAME}</title>
         </Head>
         <body>
           <Main />
